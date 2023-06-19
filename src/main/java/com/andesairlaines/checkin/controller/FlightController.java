05a2af6c67ac3a856bd9e4e33cc8e7cc20626562
Arnold-Mollo <amollo@techeraperu.com>
2023-06-18 23:57:46 -0500

    DELETE unnecessary files
    Se eliminaron archivos innecesarios para el propósito del challenge

--- src/main/java/com/andesairlaines/checkin/controller/FlightController.java
+++ src/main/java/com/andesairlaines/checkin/controller/FlightController.java
@@ -1,8 +1,6 @@
 package com.andesairlaines.checkin.controller;
 
 import com.andesairlaines.checkin.domain.flight.DataFlight;
-import com.andesairlaines.checkin.domain.flight.DataListFlight;
-import com.andesairlaines.checkin.domain.flight.Flight;
 import com.andesairlaines.checkin.domain.flight.FlightRepository;
 import com.andesairlaines.checkin.domain.response.ResponseData;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,24 +11,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/flight")
 public class FlightController {
 
     @Autowired
     private FlightRepository flightRepository;
-
-    /*@GetMapping
-    public List<DataListFlight> listFlights() {
-        return flightRepository.findAll().stream().map(DataListFlight::new).toList();
-    }*/
-
-    @GetMapping
-    public ResponseData listFlights() {
-        return new ResponseData(HttpStatus.OK.value(), flightRepository.findAll().stream().map(DataListFlight::new).toList());
-    }
 
     @GetMapping("/{id}/passengers")
     public ResponseEntity<ResponseData> flight(@PathVariable Long id) {
--- src/main/java/com/andesairlaines/checkin/domain/seat/SeatRepository.java
+++
@@ -1,9 +0,0 @@
-package com.andesairlaines.checkin.domain.seat;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface SeatRepository extends JpaRepository<Seat, Long> {
-    //List<Seat> findByAirplaneId(Long i);
-}
