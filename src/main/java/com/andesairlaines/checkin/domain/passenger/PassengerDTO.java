a8c9dc8d83a47cb45ffd8dd0c4fecce6e26fe53b
Arnold-Mollo <amollo@techeraperu.com>
2023-06-18 21:58:03 -0500

    Fix incomplete response for each passenger

--- src/main/java/com/andesairlaines/checkin/domain/passenger/PassengerDTO.java
+++ src/main/java/com/andesairlaines/checkin/domain/passenger/PassengerDTO.java
@@ -4,11 +4,11 @@ import com.andesairlaines.checkin.domain.boardingPass.BoardingPass;
 
 public record PassengerDTO(
         Long passengerId,
-        //Long dni,
-        //String name,
+        Long dni,
+        String name,
         Integer age,
-        //String country,
-        //Long boardingPassId,
+        String country,
+        Long boardingPassId,
         Long purchaseId,
         Long seatTypeId,
         Long seatId
@@ -17,11 +17,11 @@ public record PassengerDTO(
     public PassengerDTO(BoardingPass boardingPass) {
         this(
                 boardingPass.getPassenger().getPassengerId(),
-                //Long.valueOf(boardingPass.getPassenger().getDni()),
-                //boardingPass.getPassenger().getName(),
+                Long.valueOf(boardingPass.getPassenger().getDni()),
+                boardingPass.getPassenger().getName(),
                 boardingPass.getPassenger().getAge(),
-                //boardingPass.getPassenger().getCountry(),
-                //boardingPass.getBoardingPassId(),
+                boardingPass.getPassenger().getCountry(),
+                boardingPass.getBoardingPassId(),
                 boardingPass.getPurchase().getPurchaseId(),
                 boardingPass.getSeatType().getSeatTypeId(),
                 boardingPass.getSeat() != null ? boardingPass.getSeat().getSeatId() : null
@@ -31,11 +31,11 @@ public record PassengerDTO(
     public PassengerDTO copy() {
         return new PassengerDTO(
                 this.passengerId,
-                //this.dni,
-                //this.name,
+                this.dni,
+                this.name,
                 this.age,
-                //this.country,
-                //this.boardingPassId,
+                this.country,
+                this.boardingPassId,
                 this.purchaseId,
                 this.seatTypeId,
                 this.seatId
@@ -45,11 +45,11 @@ public record PassengerDTO(
     public PassengerDTO copy(Long key) {
         return new PassengerDTO(
                 this.passengerId,
-                //this.dni,
-                //this.name,
+                this.dni,
+                this.name,
                 this.age,
-                //this.country,
-                //this.boardingPassId,
+                this.country,
+                this.boardingPassId,
                 this.purchaseId,
                 this.seatTypeId,
                 key
